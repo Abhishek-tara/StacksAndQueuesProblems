2fddb963717a800500e4f7e50cdc0e1d202a7e1e
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-23 11:20:34 +0530

    Added Main Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StacksAndQueues
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Stack and Queue Problems");
+        }
+    }
+}
