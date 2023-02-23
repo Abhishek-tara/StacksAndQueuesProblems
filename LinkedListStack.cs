0075b7a5f7be929c9e68b20d661ee4235fc9159a
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-23 11:37:08 +0530

    Added Method to push value in stack

---
+++ LinkedListStack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class LinkedListStack
+    {
+        private Node head;
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.head == null)
+                node.next = null;
+            else
+                node.next = this.head;
+                this.head = node;
+                Console.WriteLine("{0} pushed to stack ", value);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Stack and Queue Problems");
+
+            Console.WriteLine("Creating Stack using Linked List");
+
+            LinkedListStack stack = new LinkedListStack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
         }
     }
 }
